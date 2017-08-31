9ebedfdb8689d32f00abaa8b8c5c398322441642
Nagarjuna <nagarjuna524@gmail.com>
2017-08-31 17:26:52 +0530

    Added extra parameters to TestRunner

--- src/cucumberTest/TestRunner.java
+++ src/cucumberTest/TestRunner.java
@@ -5,10 +5,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(
-		features = "Feature"
-		,glue={"stepDefinition"}
-		)
+@CucumberOptions(features = "Feature", glue = { "stepDefinition" }, dryRun = false, monochrome = true)
 
 public class TestRunner {
 
